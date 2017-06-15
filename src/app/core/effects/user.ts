d45771fb067e7b6dd89d9e534c49fe2a8d5f55f9
Ashish K. Mondal <ashish.mondal@halliburton.com>
2017-06-15 20:36:43 +0530

    Using effects

--- src/app/core/effects/user.ts
+++ src/app/core/effects/user.ts
@@ -1,10 +1,23 @@
-import { AuthService } from '../services/auth.service';
-import { Actions } from '@ngrx/effects';
 import { Injectable } from '@angular/core';
+import { Action } from '@ngrx/store';
+import { Actions, Effect } from '@ngrx/effects';
+import { Observable } from 'rxjs/Observable';
+import { of } from 'rxjs/observable/of';
+
+import * as user from '../actions/user';
+import { IUserInfo } from 'app/core/models/user-info';
+import { AuthService } from '../services/auth.service';
 
 @Injectable()
 export class UserEffects {
-	constructor(private action$: Actions, private authService: AuthService) {
+	@Effect()
+	signIn$: Observable<Action> = this.action$
+		.ofType(user.SIGN_IN)
+		.switchMap(() => this.authService.login())
+		.switchMap(() => this.authService.user$)
+		.map(u => !!u ? new user.SignInSuccessAction(u) : new user.SignInFailAction())
+		.catch(() => of(new user.SignInFailAction()));
 
+	constructor(private action$: Actions, private authService: AuthService) {
 	}
 }
