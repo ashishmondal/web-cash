0431c1c457e60260cb16bdaaaca3c22f77eb7b81
Ashish Kumar Mondal <ashish@mondal.in>
2017-06-25 16:14:20 +0530

    Changes to home page

--- src/app/primary/home/home.component.ts
+++ src/app/primary/home/home.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
 
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs/Observable';
+
+import * as fromRoot from 'app/core/reducers';
+import { IUserInfo } from 'app/core/models';
+
 @Component({
 	selector: 'wc-home',
 	changeDetection: ChangeDetectionStrategy.OnPush,
@@ -7,8 +13,11 @@ import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
 	styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+	user$: Observable<IUserInfo>;
 
-	constructor() { }
+	constructor(private store: Store<fromRoot.State>) {
+		this.user$ = store.select(fromRoot.getUser);
+	}
 
 	ngOnInit() {
 	}
